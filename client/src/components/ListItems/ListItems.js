c8a1169f46ef836fe8a6932dde3c02c1b79a83a0
Steven Quintana <steve.e.quint@gmail.com>
2021-10-15 15:56:26 -0700

    Create function to pass down applicant info as props to the list level divided by status type

--- client/src/components/ListItems/ListItems.js
+++ client/src/components/ListItems/ListItems.js
@@ -1,13 +1,10 @@
 import ListGroup from 'react-bootstrap/ListGroup'
 
-const ListItems = () => {
+const ListItems = ({ candidates }) => {
+  console.log(candidates)
   return (
     <ListGroup>
-      <ListGroup.Item>Cras justo odio</ListGroup.Item>
-      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      {candidates ? candidates.map(applicant => <ListGroup.Item>{applicant.email}</ListGroup.Item>) : <></>}
     </ListGroup>
   )
 }
