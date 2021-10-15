795f4ff8b82ca86872239f79d2b9ae46f9e8cb31
Steven Quintana <steve.e.quint@gmail.com>
2021-10-15 13:13:54 -0700

    Update state to user in general then pass in jobs array of user to JobCard and change to custom titles

--- client/src/pages/PostedJobs/PostedJobs.js
+++ client/src/pages/PostedJobs/PostedJobs.js
@@ -1,11 +1,30 @@
-import NavbarElem from '../../components/NavbarElem'
+import { useState, useEffect } from "react"
+import NavbarElem from "../../components/NavbarElem"
 import PageTitle from "../../components/PageTitle"
+import JobCard from "../../components/JobCard"
+import UserAPI from "../../utils/UserAPI"
 
 const PostedJobs = () => {
+  const [ user, setUser ] = useState()
+
+  useEffect(() => {
+    UserAPI.getUser()
+      .then(({ data }) => setUser(data))
+      .catch(err => window.location = '/auth')
+  }, [])
+
+  const displayTitle = _ => {
+    return `Jobs Posted by ${user.first_name}`
+  }
+
+  console.log(user)
+
   return (
     <>
       <NavbarElem />
-      <PageTitle title="Posted Jobs" />
+      <PageTitle title={displayTitle()} />
+      {user ? user.jobs.map(job => <JobCard job={job} />) : <h1>You don't have any posted jobs</h1>}
+
     </>
   )
 }
