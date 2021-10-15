c2adebb0b487b234b36b3d800b9bd981759bf86b
Steven Quintana <steve.e.quint@gmail.com>
2021-10-14 19:24:20 -0700

    On component mount get user data, extract jobs array for the user and set the jobs state with it

--- client/src/pages/AppliedJobs/AppliedJobs.js
+++ client/src/pages/AppliedJobs/AppliedJobs.js
@@ -1,7 +1,18 @@
+import { useState, useEffect } from "react"
 import NavbarElem from "../../components/NavbarElem"
-import PageTitle from "../../components/PageTitle";
+import PageTitle from "../../components/PageTitle"
+import UserAPI from "../../utils/UserAPI/UserAPI"
 
 const AppliedJobs = () => {
+  const [ jobs, setJobs ] = useState([])
+
+  // On page mount get the current user, extract their jobs array and set to state.
+  useEffect(() => {
+    UserAPI.getUser()
+      .then(({ data }) => setJobs(data.jobs))
+      .catch(err => window.location = '/auth')
+  }, [])
+
   return (
     <>
       <NavbarElem />
