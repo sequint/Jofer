7de2c29d2ae1e36b86b0524e5ff27907476f6bcc
Steven Quintana <steve.e.quint@gmail.com>
2021-10-18 12:26:10 -0700

    Create input section for user to type names for filtering

---
+++ client/src/components/ApplicantFilter/ApplicantFilter.js
@@ -0,0 +1,11 @@
+import './ApplicantFilter.css'
+
+const ApplicantFilter = () => {
+  return(
+    <>
+      <input type="text" className="filter" placeholder="Filter Applicants" />
+    </>
+  )
+}
+
+export default ApplicantFilter
