e9abd1a9f919c056eb52fe7d5025064814ffe16a
Steven Quintana <steve.e.quint@gmail.com>
2021-10-18 12:23:35 -0700

    Delete log for testing

--- client/src/components/ListItems/ListItems.js
+++ client/src/components/ListItems/ListItems.js
@@ -1,7 +1,6 @@
 import ListGroup from 'react-bootstrap/ListGroup'
 
 const ListItems = ({ applicants }) => {
-  console.log(applicants)
   return (
     <ListGroup>
       {applicants ? applicants.map(applicant => <ListGroup.Item>{applicant.applicantName}</ListGroup.Item>) : <></>}
