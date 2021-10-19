06164ea4aafd13cef7210220fe36f29d2813fdc8
Paul Pearce <pearce.o.paul2020@gmail.com>
2021-10-19 11:28:53 -0700

    Added Dynamic Log In/Out Button

--- client/src/components/NavbarElem/NavbarElem.js
+++ client/src/components/NavbarElem/NavbarElem.js
@@ -2,32 +2,43 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import logo from '../../assets/jobrejectorlogo.png'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+
 const NavbarElem = () => {
+ 
+  const IsLoggedIn = localStorage.getItem("token")
+  
+  const handleSignOut = () => {
+    localStorage.removeItem("token");
+    window.location = "/login";
+  }
+
   return (
-    <Navbar bg='dark' variant='dark' expand='lg'>
-      <img src={logo} alt='' width='70' />
-      <Navbar.Brand alt='' href='#home'>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <img src={logo} alt="" width="70" />
+      <Navbar.Brand alt="" href="#home">
         Job Rejector
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className='me-auto'>
-          <Nav.Link href='/'>Home</Nav.Link>
-          <NavDropdown title='Jobs' id='basic-nav-dropdown'>
-            <NavDropdown.Item href='/appliedjobs'>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <NavDropdown title="Jobs" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/appliedjobs">
               Applied Jobs
             </NavDropdown.Item>
-            <NavDropdown.Item href='/declinedjobs'>
-              Declined Jobs
-            </NavDropdown.Item>
-            <NavDropdown.Item href='/managejobs'>Manage Jobs</NavDropdown.Item>
-            <NavDropdown.Item href='/postedjobs'>Posted Jobs</NavDropdown.Item>
+            <NavDropdown.Item href="/postedjobs">Posted Jobs</NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link href='/login'>Login</Nav.Link>
+          {IsLoggedIn ? (
+            <Nav.Link onClick={handleSignOut} href="/login">
+              Log Out
+            </Nav.Link>
+          ) : (
+            <Nav.Link href="/login">Log In</Nav.Link>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
 
 export default NavbarElem
