06166c95b42062598ddf03b09d339df6e6acd7e0
Steven Quintana <steve.e.quint@gmail.com>
2021-10-19 11:38:01 -0700

    Add custom text

--- client/src/components/DeclineModal/DeclineModal.js
+++ client/src/components/DeclineModal/DeclineModal.js
@@ -15,18 +15,20 @@ const DeclineModal = () => {
         backdrop="static"
         keyboard={false}
       >
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+        <Modal.Header>
+          <Modal.Title>Decline Rational</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          I will not close if you click outside me. Don't even try to press
-          escape key.
+          <h3>Decline Reasons</h3>
+          <p>Please add at least one decline reason below.</p>
+          <h3>Action Items</h3>
+          <p>Please list a minimum of 3 skill sets the applicant can imporove on to be better prepared for a similar position in the future.</p>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
+          <Button variant="warning" onClick={handleClose}>
+            Revert Decline Status
           </Button>
-          <Button variant="primary">Understood</Button>
+          <Button variant="danger">Decline</Button>
         </Modal.Footer>
       </Modal>
     </>
