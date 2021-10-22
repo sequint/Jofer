1d4b33285ad24a502e5a60ce1111f8c2b0a981a4
Polyca <polyca123@gmail.com>
2021-10-22 11:12:46 -0700

    applied Job

--- client/src/components/AppliedJobCard/AppliedJobCard.js
+++ client/src/components/AppliedJobCard/AppliedJobCard.js
@@ -3,6 +3,7 @@ import Modal from 'react-bootstrap/Modal'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import JobAPI from '../../utils/JobAPI'
+import UserAPI from '../../utils/UserAPI'
 import './AppliedJobCard.css'
 
 const AppliedJobCard = ({ job }) => {
@@ -25,6 +26,15 @@ const AppliedJobCard = ({ job }) => {
         data.forEach(elem => {
           if (elem._id === job.jobID) {
             console.log(elem)
+            UserAPI.getUser()
+              .then(({ data }) => {
+                elem.applicants.forEach(applicant => {
+                  if (applicant.email === data.email) {
+                    console.log(applicant.email)
+                    setDeclinedReasons({ reasons: applicant.declined.reasons, actionItems: applicant.declined.actionItems })
+                  }
+                })
+              })
             // let tempJob = elem.applicants.filter(applicant => applicant.email === job)
             // setDeclinedReasons({ ...declinedReasons, reasons: elem.applicants.declined.reasons, actionItems: elem.applicants.declined.actionItems })
           }
@@ -32,55 +42,55 @@ const AppliedJobCard = ({ job }) => {
       })
   }, [])
 
-  
 
 
-// const saveToLocal = event => {
-//   event.preventDefault()
 
-//   localStorage.setItem('clickedManageJob', JSON.stringify(job))
-// }
+  // const saveToLocal = event => {
+  //   event.preventDefault()
 
-return (
-  <>
-    <Card className='m-2'>
-      <Card.Header className='status' as='h5'>{job.status}</Card.Header>
-      <Card.Body>
-        <Card.Title>{job.name}</Card.Title>
-        <Card.Text>
-          Company: {job.company}
-        </Card.Text>
-        <Card.Text>
-          Department: {job.type}
-        </Card.Text>
-        <Button
-          variant='outline-secondary'
-          onClick={handleShow}>
-          View More
-        </Button>
-      </Card.Body>
-    </Card>
-    <Modal
-      show={show}
-      onHide={handleClose}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered>
-      <Modal.Header closeButton>
-        <Modal.Title>
-          {job.name}
-        </Modal.Title>
-      </Modal.Header>
+  //   localStorage.setItem('clickedManageJob', JSON.stringify(job))
+  // }
 
-      <Modal.Body>
-        <p><strong>Status:</strong> {job.status}</p>
-        <p><strong>Company:</strong> {job.company}</p>
-        <p><strong>Department:</strong> {job.type}</p>
-        <p><strong>Department:</strong> {job.type}</p>
-      </Modal.Body>
-    </Modal>
-  </>
-)
+  return (
+    <>
+      <Card className='m-2'>
+        <Card.Header className='status' as='h5'>{job.status}</Card.Header>
+        <Card.Body>
+          <Card.Title>{job.name}</Card.Title>
+          <Card.Text>
+            Company: {job.company}
+          </Card.Text>
+          <Card.Text>
+            Department: {job.type}
+          </Card.Text>
+          <Button
+            variant='outline-secondary'
+            onClick={handleShow}>
+            View More
+          </Button>
+        </Card.Body>
+      </Card>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {job.name}
+          </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p><strong>Status:</strong> {job.status}</p>
+          <p><strong>Company:</strong> {job.company}</p>
+          <p><strong>Department:</strong> {job.type}</p>
+          <p><strong>Department:</strong> {job.type}</p>
+        </Modal.Body>
+      </Modal>
+    </>
+  )
 }
 
 export default AppliedJobCard
