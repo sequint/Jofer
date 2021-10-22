f55bc36dc3cad8bbd6891b65a4bdbfce670395ff
Polyca <polyca123@gmail.com>
2021-10-21 19:48:19 -0700

    applied Job Card adjusted

--- client/src/components/AppliedJobCard/AppliedJobCard.js
+++ client/src/components/AppliedJobCard/AppliedJobCard.js
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Modal from 'react-bootstrap/Modal'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import JobAPI from '../../utils/JobAPI'
 import './AppliedJobCard.css'
 
 const AppliedJobCard = ({ job }) => {
@@ -13,52 +14,73 @@ const AppliedJobCard = ({ job }) => {
   const handleShow = () => setShow(true)
   const handleClose = () => setShow(false)
 
-  // const saveToLocal = event => {
-  //   event.preventDefault()
+  const [declinedReasons, setDeclinedReasons] = useState({
+    reasons: [],
+    actionItems: []
+  })
 
-  //   localStorage.setItem('clickedManageJob', JSON.stringify(job))
-  // }
+  useEffect(() => {
+    JobAPI.getEmployerJobs()
+      .then(({ data }) => {
+        data.forEach(elem => {
+          if (elem._id === job.jobID) {
+            console.log(elem)
+            // let tempJob = elem.applicants.filter(applicant => applicant.email === job)
+            // setDeclinedReasons({ ...declinedReasons, reasons: elem.applicants.declined.reasons, actionItems: elem.applicants.declined.actionItems })
+          }
+        })
+      })
+  }, [])
 
-  return (
-    <>
-      <Card className='m-2'>
-        <Card.Header className='status' as='h5'>{job.status}</Card.Header>
-        <Card.Body>
-          <Card.Title>{job.name}</Card.Title>
-          <Card.Text>
-            Company: {job.company}
-          </Card.Text>
-          <Card.Text>
-            Department: {job.type}
-          </Card.Text>
-          <Button
-            variant='outline-secondary'
-            onClick={handleShow}>
-            View More
-          </Button>
-        </Card.Body>
-      </Card>
-      <Modal
-        show={show}
-        onHide={handleClose}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered>
-        <Modal.Header closeButton>
-          <Modal.Title>
-            {job.name}
-          </Modal.Title>
-        </Modal.Header>
+  
 
-        <Modal.Body>
-          <p><strong>Status:</strong> {job.status}</p>
-          <p><strong>Company:</strong> {job.company}</p>
-          <p><strong>Department:</strong> {job.type}</p>
-          <p><strong>Department:</strong> {job.type}</p>
-        </Modal.Body>
-      </Modal>
-    </>
-  )
+
+// const saveToLocal = event => {
+//   event.preventDefault()
+
+//   localStorage.setItem('clickedManageJob', JSON.stringify(job))
+// }
+
+return (
+  <>
+    <Card className='m-2'>
+      <Card.Header className='status' as='h5'>{job.status}</Card.Header>
+      <Card.Body>
+        <Card.Title>{job.name}</Card.Title>
+        <Card.Text>
+          Company: {job.company}
+        </Card.Text>
+        <Card.Text>
+          Department: {job.type}
+        </Card.Text>
+        <Button
+          variant='outline-secondary'
+          onClick={handleShow}>
+          View More
+        </Button>
+      </Card.Body>
+    </Card>
+    <Modal
+      show={show}
+      onHide={handleClose}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          {job.name}
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <p><strong>Status:</strong> {job.status}</p>
+        <p><strong>Company:</strong> {job.company}</p>
+        <p><strong>Department:</strong> {job.type}</p>
+        <p><strong>Department:</strong> {job.type}</p>
+      </Modal.Body>
+    </Modal>
+  </>
+)
 }
 
 export default AppliedJobCard
