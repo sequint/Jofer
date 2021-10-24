c6ab10c8162accde9bc053c95a3e9fa652215152
Gilberto Resendiz <gresendi@uci.edu>
2021-10-24 12:35:57 -0700

    changed DropdownButton import statement

--- client/src/pages/AppliedJobs/AppliedJobs.js
+++ client/src/pages/AppliedJobs/AppliedJobs.js
@@ -4,7 +4,7 @@ import PageTitle from '../../components/PageTitle'
 import JobAPI from '../../utils/JobAPI'
 import AppliedJobCard from '../../components/AppliedJobCard/AppliedJobCard'
 import UserAPI from '../../utils/UserAPI'
-import {DropdownButton} from 'react-bootstrap/Dropdown'
+import {DropdownButton} from 'react-bootstrap'
 import Container from 'react-bootstrap/esm/Container'
 import './AppliedJobs.css'
 
