5b5b3568410c0b3f49c21b4951ba730cdd54c61c
Gilberto Resendiz <gresendi@uci.edu>
2021-10-24 18:33:18 -0700

    added seed routes

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 
 router.use('/api', require('./jobRoutes.js'))
 router.use('/api', require('./userRoutes.js'))
+router.use('/seed', require('./seedRoutes.js'))
 
 module.exports = router
