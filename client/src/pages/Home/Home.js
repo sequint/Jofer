3d9d3bfc24ff6745d2243cac7a31f7ddb3a5e36d
Polyca <polyca123@gmail.com>
2021-10-24 21:11:30 -0700

    navbar avatar moved

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -9,50 +9,49 @@ const Home = () => {
 
 
   const [jobs, setJobs] = useState([{
-   
+
   }])
   const [filteredJobs, setFilteredJobs] = useState([{
-   
+
   }])
 
-  useEffect(() => { 
+  useEffect(() => {
     JobAPI.getAllJobs()
       .then(({ data }) => {
         let jobs = []
-        let applicants =[]
-        data.forEach(job=>{
-          job.applicants.forEach(applicant=>{
-            if(applicant.status=='Declined')
-            {           
+        let applicants = []
+        data.forEach(job => {
+          job.applicants.forEach(applicant => {
+            if (applicant.status == 'Declined') {
               applicants.push(applicant)
             }
           })
-          if(applicants.length!==0){
-            job.status= "Declined"
+          if (applicants.length !== 0) {
+            job.status = "Declined"
             job.applicants = applicants
             applicants = []
             jobs.push(job)
           }
-        
+
         })
         setJobs(jobs)
-        setFilteredJobs(jobs)     
+        setFilteredJobs(jobs)
       })
       .catch(err => console.log('err'))
 
     if (!localStorage.getItem("token")) {
       window.location = "/login"
     }
-    
+
     if (jobs.length > 0) {
       setFilteredJobs(jobs)
     }
 
   }, [])
 
-  const handleInputChange = ({ target: { value,name } }) => {
-    
-    switch(name){
+  const handleInputChange = ({ target: { value, name } }) => {
+
+    switch (name) {
       case "filterCompany":
         const company = jobs.filter(department => department.company.substring(0, value.length).toUpperCase() === value.toUpperCase())
         setFilteredJobs(company)
@@ -61,9 +60,9 @@ const Home = () => {
         const department = jobs.filter(department => department.type.substring(0, value.length).toUpperCase() === value.toUpperCase())
         setFilteredJobs(department)
         break
-        default:
-          break
-    }   
+      default:
+        break
+    }
   }
 
 
@@ -71,23 +70,25 @@ const Home = () => {
   return (
     <>
       <NavbarElem />
-      
-      <PageTitle title='Welcome User' />
 
-      <input
-        type="text"
-        name="filterCompany"
-        className="filter"
-        placeholder="Search company"
-        onChange={handleInputChange}
-      />
-      <input
-        type="text"
-        name="filterDepartment"
-        className="filter"
-        placeholder="Search department"
-        onChange={handleInputChange}
-      />
+      <PageTitle title='Welcome User' />
+      
+      <div>
+        <input
+          type="text"
+          name="filterCompany"
+          className="filter mb-2"
+          placeholder="Search company"
+          onChange={handleInputChange} />
+
+        <input
+          type="text"
+          name="filterDepartment"
+          className="filter"
+          placeholder="Search department"
+          onChange={handleInputChange} />
+      </div>
+
       {filteredJobs ? filteredJobs.slice(0).reverse().map(job => <HomeCard job={job}></HomeCard>) : <h1>You don't have any posted jobs</h1>}
     </>
   )
