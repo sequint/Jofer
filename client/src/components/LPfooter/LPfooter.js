c0fc0c291b723b5f8ac9e496eb74e0dd52da4330
Polyca <polyca123@gmail.com>
2021-11-05 15:23:48 -0700

    landing page skeleton done

---
+++ client/src/components/LPfooter/LPfooter.js
@@ -0,0 +1,13 @@
+const LPfooter = () => {
+  return (
+    <footer className="py-5 bg-dark">
+      <div className="container px-5">
+        <p className="m-0 text-center text-white">
+          something really neat here.
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default LPfooter
