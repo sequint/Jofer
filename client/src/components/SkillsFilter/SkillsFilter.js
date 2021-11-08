2e41f525d09318bf055983c97d5f0a42d4a02b19
Steven Quintana <steve.e.quint@gmail.com>
2021-11-08 10:20:18 -0800

    Create show and close handlers

--- client/src/components/SkillsFilter/SkillsFilter.js
+++ client/src/components/SkillsFilter/SkillsFilter.js
@@ -1,8 +1,98 @@
+import { useState } from 'react'
+import {
+  Button,
+  Modal,
+  Row,
+  Form
+} from 'react-bootstrap'
 import './SkillsFilter.css'
 
 const SkillsFilter = () => {
+  // Define all state variables for the component.
+  const [ show, setShow ] = useState(false)
+
+  // Show and close modal handlers.
+  // Modal show handlers.
+  const handleClose = () => {
+    // Reset all states and close the modal.
+    setShow(false)
+  }
+  const handleShow = () => setShow(true)
+
   return(
     <>
+      <div
+        className="mt-2 mb-2 createNewJob">
+        <Button
+          className="col-2 CreateJobBtn"
+          varient="outline-secondary"
+          onClick={handleShow}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16">
+            <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
+          </svg>
+          Filter by Skill
+        </Button>
+      </div>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Add Skills
+          </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Row>
+            <Form>
+              <Form.Group className='mb-3' controlId='applicantName'>
+                <Form.Label>Skill Name</Form.Label>
+                <Form.Control
+                  className="gray"
+                  type='text'
+                  placeholder='Enter a skill'
+                  name='skill'
+                  value={skillState.skill}
+                  onChange={handleInputChange}
+                />
+                {skillState.missingSkill ? <p className="err mt-2">⚠️ Please enter a skill</p> : <></>}
+              </Form.Group>
+              <Button
+                className="mt-3 createBtn"
+                variant='primary'
+                type='submit'
+                onClick={handleAddSkill}
+              >
+                Add Skill
+              </Button>
+            </Form>
+          </Row>
+
+          <hr />
+
+          <Row>
+            <h3>Skills</h3>
+            {skillState.allSkills.length > 0 ? skillState.allSkills.map(skill => <li>{skill}</li>) : <></>}
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          {skillState.missingAllSkills > 0 ? <p className="err me-4">⚠️ No skills added yet</p> : <></>}
+          <Button
+            className="createBtn"
+            variant='primary'
+            type='submit'
+            onClick={handleAddAllSkills}
+          >
+            Add New Skills
+          </Button>
+        </Modal.Footer>
+
+      </Modal>
 
     </>
   )
