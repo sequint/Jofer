bcd9465b43ca70baf6e545a5e7b27cace16dbb29
Polyca <polyca123@gmail.com>
2021-11-09 19:48:07 -0800

    bio changed and small adjustments made

--- client/src/components/LPfooter/LPfooter.js
+++ client/src/components/LPfooter/LPfooter.js
@@ -2,8 +2,8 @@ const LPfooter = () => {
   return (
     <footer className="py-5 bg-dark">
       <div className="container px-5">
-        <p className="m-0 text-center text-white">     
-            &copy; Jofer 2021 Created by  Steven Quintana, Gilberto Resendiz, Minh Nguyen
+        <p className="m-0 text-center text-white">
+          &copy; Jofer 2021 Created by  Steven Quintana, Gilberto Resendiz and Minh Nguyen
         </p>
       </div>
     </footer>
